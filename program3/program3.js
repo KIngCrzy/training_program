2d513f4e18fb7044397af4241dfc851cac339171
Crazy <392629255@qq.com>
2019-05-17 15:41:05 +0800

    通过命令行参数控制端口

--- program3/program3.js
+++ program3/program3.js
@@ -8,11 +8,10 @@ const app = new Koa()
 const router = new Router()
 let random = 0
 let random1 = 0
-let num = 0
 
 router.get('/start', async (ctx) => {
 	if (random === 0) {
-		random = parseInt(Math.random() * 100, 10)
+		random = Math.floor((Math.random() * 100, 10))
 	}
 	client.set('random', random)
 	client.get('random', (err, v) => {
@@ -24,14 +23,14 @@ router.get('/start', async (ctx) => {
 })
 
 router.get('/:number', async (ctx) => {
-	num = ctx.params.number
+	const num = ctx.params.number
 	console.log(num)
 	if (num > random1) {
 		ctx.response.body = 'bigger'
 	}
 	if (num === random1) {
 		ctx.response.body = 'equal'
-		random = parseInt(Math.random() * 100, 10)
+		random = Math.floor((Math.random() * 100, 10))
 		console.log(random)
 	}
 	if (num < random1) {
@@ -44,6 +43,4 @@ client.on('error', (error) => {
 })
 
 app.use(router.routes()).use(router.allowedMethods())
-app.listen(8080)
-app.listen(80)
-app.listen(8888)
+app.listen(process.argv[2])
