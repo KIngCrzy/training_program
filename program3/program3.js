1fe9641ab151a56587905993ca792b6e1226943c
Crazy <392629255@qq.com>
2019-05-22 11:49:53 +0800

    实现多个程序同时对一个R进行比较

--- program3/program3.js
+++ program3/program3.js
@@ -1,45 +1,63 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const redis = require('redis')
+const util = require('util')
 
 const client = redis.createClient()
 
 const app = new Koa()
 const router = new Router()
 let random = 0
-let random1 = 0
+let num = 0
+let state = 0
+
+const getrandom = util.promisify(client.get).bind(client)
+
+function compare(r) {
+	console.log('random:', r)
+	if (num > r) {
+		state = 0
+	}
+	if (num === r) {
+		state = 1
+	}
+	if (num < r) {
+		state = 2
+	}
+}
 
 router.get('/start', async (ctx) => {
 	if (random === 0) {
-		random = Math.floor((Math.random() * 100, 10))
+		random = Math.floor((Math.random() * 100 + 1))
 	}
 	client.set('random', random)
 	client.get('random', (err, v) => {
-		random1 = v
-		console.log(random1)
+		console.log('random', v)
 	})
-	console.log(random)
 	ctx.response.body = 'ok'
 })
 
 router.get('/:number', async (ctx) => {
-	const num = ctx.params.number
-	console.log(num)
-	if (num > random1) {
+	num = ctx.params.number
+	console.log('num:', num)
+	const random1 = await getrandom('random')
+	compare(random1)
+	switch (state) {
+	case 0:
 		ctx.response.body = 'bigger'
-	}
-	if (num === random1) {
+		break
+	case 1:
 		ctx.response.body = 'equal'
-		random = Math.floor((Math.random() * 100, 10))
-		console.log(random)
-	}
-	if (num < random1) {
+		random = Math.floor((Math.random() * 100 + 1))
+		client.set('random', random)
+		console.log('new random:', random)
+		break
+	case 2:
 		ctx.response.body = 'smaller'
+		break
+	default:
+		break
 	}
-})
-
-client.on('error', (error) => {
-	console.log(error)
 })
 
 app.use(router.routes()).use(router.allowedMethods())
